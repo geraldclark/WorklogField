bfff6cfde77c5961fc255408ee864bf2b6d50c9b
Jerry Clark <geraldbclark@gmail.com>
2014-10-07 17:04:17 -0400

    Update TemplateWorklog.php

--- InstallerPackage/copy/custom/modules/DynamicFields/templates/Fields/TemplateWorklog.php
+++ InstallerPackage/copy/custom/modules/DynamicFields/templates/Fields/TemplateWorklog.php
@@ -1,15 +1,7 @@
 <?php
+
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*
- * Your installation or use of this SugarCRM file is subject to the applicable
- * terms available at
- * http://support.sugarcrm.com/06_Customer_Center/10_Master_Subscription_Agreements/.
- * If you do not agree to all of the applicable terms or do not have the
- * authority to bind the entity as an authorized representative, then do not
- * install or use this SugarCRM file.
- *
- * Copyright (C) SugarCRM Inc. All rights reserved.
- */
+
 require_once('modules/DynamicFields/templates/Fields/TemplateText.php');
 class TemplateWorklog extends TemplateText{
     var $type = 'text';
